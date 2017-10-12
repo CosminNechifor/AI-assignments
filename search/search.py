ca7359b5fe34228281e973f32353c848c953a992
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2017-10-12 13:36:55 +0300

    So it has begun.

--- search/search.py
+++ search/search.py
@@ -72,22 +72,10 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
+
+# DFS implementation for pacman
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
